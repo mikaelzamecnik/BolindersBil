a19d47796feabf7e829c5d536a395af65a2c85a8
Mikael Zamecnik <zami1783@student.ju.se>
2018-09-25 14:26:13 +0200

    Api almost works

---
+++ BolindersBil.Web/Constants/ErrorCodes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BolindersBil.Web.Constants
+{
+    public enum ErrorCodes
+    {
+        ApiKeyExhausted,
+        ApiKeyMissing,
+        ApiKeyInvalid,
+        ApiKeyDisabled,
+        ParametersMissing,
+        ParametersIncompatible,
+        ParameterInvalid,
+        RateLimited,
+        RequestTimeout,
+        SourcesTooMany,
+        SourceDoesNotExist,
+        SourceUnavailableSortedBy,
+        SourceTemporarilyUnavailable,
+        UnexpectedError,
+        UnknownError
+    }
+}
---
+++ BolindersBil.Web/Controllers/NewsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BolindersBil.Web.Constants;
+using BolindersBil.Web.Infrastructure;
+using BolindersBil.Web.Models;
+using BolindersBil.Web.Models.NewsModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BolindersBil.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+
+
+
+        public IActionResult Index(ArticlesResult articlesResult)
+        {
+
+            var newsApiClient = new NewsApiClient("e6e66d30aaba4a1b8ad9c4a17a1a4117");
+
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "Apple",
+                SortBy = SortBys.PublishedAt,
+                Language = Languages.EN,
+                From = new DateTime(2018, 09, 24)
+            });
+
+
+                return View(articlesResponse.TotalResults);
+
+
+
+
+
+
+
+        }
+    }
+}
