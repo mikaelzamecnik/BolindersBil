cf191d75161d3e976412350b6091a0a20a606503
Mikael Zamecnik <mikael.zamecnik@gmail.com>
2018-09-30 17:36:26 +0200

    added body in filterlistitems

--- BolindersBil.Web/Infrastructure/FilterListItems.cs
+++ BolindersBil.Web/Infrastructure/FilterListItems.cs
@@ -8,10 +8,20 @@ using System.Threading.Tasks;
 namespace BolindersBil.Web.Infrastructure
 {
 
-    public class FilterListItems
+    public static class FilterListItems
     {
-
-        // Adding logic for FIlterlistItems
+        public static List<SelectListItem> Bodies { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "Småbil", Text = "Småbil" },
+            new SelectListItem { Value = "Sedan", Text = "Sedan" },
+            new SelectListItem { Value = "Halvkombi", Text = "Halvkombi"  },
+            new SelectListItem { Value = "Kombi", Text = "Kombi"  },
+            new SelectListItem { Value = "SUV", Text = "SUV"  },
+            new SelectListItem { Value = "Coupé", Text = "Coupé"  },
+            new SelectListItem { Value = "Cab", Text = "Cab"  },
+            new SelectListItem { Value = "Familjebuss", Text = "Familjebuss"  },
+            new SelectListItem { Value = "Yrkesfordon", Text = "Yrkesfordon"  },
+        };
     }
     
        
