1dd9127b1dc243c15465fc519579ba8a276291a5
Mikael Zamecnik <zami1783@student.ju.se>
2018-10-03 08:20:48 +0200

    required

--- BolindersBil.Web/Models/Vehicle.cs
+++ BolindersBil.Web/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,17 +10,27 @@ namespace BolindersBil.Web.Models
     public class Vehicle
     {
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Du måste välja en bilmodell!")]
         public string Model { get; set; }
+        [Required(ErrorMessage = "Du måste välja modellbeskrivning!")]
         public string ModelDescription { get; set; }
+        [Required(ErrorMessage = "Fordonet måste ha registeringsnummer!")]
         public string RegistrationNumber { get; set; }
+        [Required(ErrorMessage = "Fordonet måste ha en årsmodell!")]
         public string Year { get; set; }
+        [Required(ErrorMessage = "Fordonet måste ha kilometertal!")]
         public double Mileage { get; set; }
+        [Required(ErrorMessage = "Fordonet måste ha ett pris!")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Du måste välja vilken kaross bilen har!")]
         public string Body { get; set; }
+        [Required(ErrorMessage = "Fordonet måste ha en färg!")]
         public string Color { get; set; }
+        [Required(ErrorMessage = "Du måste välja växellåda!")]
         public string Transmission { get; set; }
+        [Required(ErrorMessage = "Du måste välja bränsle!")]
         public string Fuel { get; set; }
+        [Required(ErrorMessage = "Du måste välja antalet hästkrafter!")]
         public string Horsepower { get; set; }
         public bool Used { get; set; }
         public string ImageUrl { get; set; }
