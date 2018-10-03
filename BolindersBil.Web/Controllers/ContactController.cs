4ee37b13b9f55a9e17b5f96bb0db296c188f6f8e
Mikael Zamecnik <zami1783@student.ju.se>
2018-10-03 16:35:05 +0200

    Master with contactform

--- BolindersBil.Web/Controllers/ContactController.cs
+++ BolindersBil.Web/Controllers/ContactController.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BolindersBil.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
 
 namespace BolindersBil.Web.Controllers
 {
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
+
+        public IActionResult SendMail(string name, string title, string email, string msg, string dealershipEmail)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("bolindersbil@hotmail.com"));
+            message.To.Add(new MailboxAddress(dealershipEmail));
+
+            message.Subject = title;
+            message.Body = new TextPart("html")
+            {
+                Text = "Från: " + name + "<br>" +
+                "Kontakt e-post: " + email + "<br>" +
+                "Meddelande: " + msg
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.office365.com", 587);
+                client.Authenticate("bolindersbil@hotmail.com", "123456wu17");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            ModelState.Clear();
+            return View("Index");
+        }
+
     }
 }
